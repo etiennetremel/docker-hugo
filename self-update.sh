f8d15506b20e3955942b8d98a954a3c21c7c84c6
Etienne Tremel <etienne.tremel@container-solutions.com>
2018-02-05 23:13:19 +0100

    Add self-update

---
+++ self-update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Self bump version
+
+repo="gohugoio/hugo"
+
+current=$(curl https://api.github.com/repos/${repo}/releases/latest -H 'Accept: application/vnd.github.v3+json' | jq -r '.tag_name')
+
+tag=${current/v/}
+
+git tag | grep $tag
+
+if [ $? -eq 0 ]
+then
+  echo "Tag already exist"
+  exit 0
+fi
+
+echo "Bumping version to ${tag}"
+
+sed 's/HUGO_VERSION=[0-9.]*/HUGO_VERSION='"${tag}"'/' Dockerfile
+
+git add Dockerfile
+git commit -m "Bump ${tag}"
+git tag $tag
+
+echo "${GIT_PRIVATE_KEY}" | base64 -d > git_rsa
+
+export GIT_SSH_COMMAND="ssh -i $PWD/git_rsa"
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name "Travis CI"
+git push origin master
+git push origin $tag
